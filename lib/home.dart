29a06f977e2b37a08e1ccccf04c8a53f7d267460
Amr7Gamal <1937.Amr@#1379>
2022-08-14 17:03:27 +0200

    done create app

---
+++ lib/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatelessWidget {
+static const String nameKey = "home";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
